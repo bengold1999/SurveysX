0e13ae2f9ce12cc6b46a62cd12096624f4a57dbd
bengold1999 <bengold1999@gmail.com>
2024-09-17 15:29:26 +0300

    add List

---
+++ src/components/SurvList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+type Props = {}
+
+export default function survList (props: Props){
+  return (
+    <div>survList</div>
+  )
+}
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import SurvList from '../components/survList'
 type Props = {}
 
 const Home = (props: Props) => {
   return (
     <div><h1>Home</h1>
     <a href="/FormBuilder">FormBuilder</a>
+    <section>
+   <SurvList></SurvList>
+    </section>
     </div>
   )
 }
