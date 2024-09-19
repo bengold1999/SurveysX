5aeb60ce8073e29aa9eeda0be6231ef94993d1d6
bengold1999 <bengold1999@gmail.com>
2024-09-19 18:41:48 +0300

    Add home base commponnet

---
+++ src/components/home/CallToAction.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type Props = {}
+
+const CallToAction = (props: Props) => {
+  return (
+    <article className='bg-primary w-full h-full flex justify-between pb-10 pt-10'>
+      <div className='w-full h-full flex flex-col justify-space-around p-20 gap-3  '>
+        <h1>Call To Action</h1>
+        <h1>hello</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, culpa.</p>
+        <button className='w-1/4'>Start Now</button>
+      </div>
+      <img className='w-1/2' src="https://picsum.photos/200" alt="sds" />
+    </article>
+  )
+}
+
+export default CallToAction
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import HeaderHome from '@/components/headerHome'
+import HeaderHome from '@/components/HeaderHome'
+import CallToAction from '@/components/home/CallToAction'
 // import HeaderHome from '@/components/HeaderHome'
 type Props = {}
 
 const Home = (props: Props) => {
   return (
     <>
-    <main className='flex flex-col'>
+    <main className='flex flex-col w-full h-full '>
     <HeaderHome/>
-    <a href="/FormBuilder">FormBuilder</a>
-    <section>
-    
+    {/* <a href="/FormBuilder">FormBuilder</a> */}
+    <section className='w-full h-full '>
+    <CallToAction></CallToAction>
+    {/* <h1>sdsad</h1> */}
     </section>
     </main>
     </>
